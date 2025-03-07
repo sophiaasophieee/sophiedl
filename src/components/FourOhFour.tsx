3608e3a3d45a922d59ac67de0df91555cf41d8c2
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-07 14:12:40 -0500

    round

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -4,7 +4,7 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image className="rounded-lg" src="/images/mega.png" alt="404" width={912} height={912} priority />
+        <Image className="customrounded" src="/images/mega.png" alt="404" width={912} height={912} priority />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
