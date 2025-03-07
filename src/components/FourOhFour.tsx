50c4ffaa9ecc1bc216b6a5aa247ae78a6c66549b
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-07 13:50:46 -0500

    me when round

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image src="/images/mega.png" alt="404" width={912} height={912} priority />
+        <Image
+          className="rounded-lg"
+          src="/images/mega.png"
+          alt="404"
+          width={912}
+          height={912}
+          priority
+        />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
@@ -12,7 +19,7 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FourOhFour
+export default FourOhFour;
