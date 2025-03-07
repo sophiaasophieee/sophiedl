f806cca30143fd433de55f55caaa48419d481294
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-07 13:49:51 -0500

    page doesn't exist

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -8,26 +8,7 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          Damn.
-        </div>
-        <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
-          {errorMsg}
-        </div>
-        <div className="text-sm">
-          Press{' '}
-          <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
-            F12
-          </kbd>{' '}
-          and open devtools for more details, and report this issue to{' '}
-          <a
-            className="text-blue-600 hover:text-blue-700 hover:underline"
-            href="https://github.com/lyc8503/onedrive-cf-index-ng/issues"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            onedrive-cf-index-ng issues
-          </a>
-          .
+          Damn. Page doesn't exist.
         </div>
       </div>
     </div>
