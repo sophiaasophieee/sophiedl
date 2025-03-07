42b719acbe359239221a7ccea1c3a9d19d4454fe
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-07 14:09:05 -0500

    i hate you website

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -4,11 +4,11 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image src="/images/mega.png" alt="404" width={912} height={912} priority className="rounded-lg" />
+        <Image className="rounded-lg" src="/images/mega.png" alt="404" width={912} height={912} priority />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          Damn. Page doesn't exist.
+          Damn, that&apos;s a 404.
         </div>
         <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
           {errorMsg}
