8906385af2137db46c65f4e3343a041d8d149693
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-07 14:04:14 -0500

    oops

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -1,25 +1,26 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image
-          className="rounded-lg"
-          src="/images/mega.png"
-          alt="404"
-          width={912}
-          height={912}
-          priority
-        />
+        <Image src="/images/mega.png" alt="404" width={912} height={912} priority className="rounded-lg" />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
           Damn. Page doesn't exist.
         </div>
+        <div className="text-sm">
+          Press{' '}
+          <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
+            F12
+          </kbd>{' '}
+          and open devtools for more details.
+          .
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FourOhFour;
+export default FourOhFour
