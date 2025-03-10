c0b09c5a8ac9cda79f9d4139bcf993c58b2e0dd7
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-10 09:38:59 -0400

    Revert "megamind :trollface:"
    This reverts commit 26ee4851d94408407eefd8cc2a5f8e6be5566796.

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -4,11 +4,11 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image className="customrounded" src="/images/mega.png" alt="404" width={912} height={912} priority />
+        <Image src="/images/fabulous-rip-2.png" alt="404" width={912} height={912} priority />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          Damn, that&apos;s a 404.
+          Oops, that&apos;s a <span className="underline decoration-red-500 decoration-wavy">four-oh-four</span>.
         </div>
         <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
           {errorMsg}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,14 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import siteConfig from '../../config/site.config'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <meta name="description" content="OneDrive Vercel Index" />
-          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="" />
+          <link rel="icon" href="/icons/sophie.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          {siteConfig.googleFontLinks.map(link => (
-            <link key={link} rel="stylesheet" href={link} />
-          ))}
         </Head>
         <body>
           <Main />
