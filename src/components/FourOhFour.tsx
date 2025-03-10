048fd9e5134688c5692757773a433c86f4eb8968
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-10 09:37:05 -0400

    Revert "me when i fix things"
    This reverts commit 6aef87324e0df866105c2525acec8c770a4e4c50.

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
-    <div className="text-center my-12">
+    <div className="my-12">
       <div className="mx-auto w-1/3">
         <Image className="customrounded" src="/images/mega.png" alt="404" width={912} height={912} priority />
       </div>
@@ -10,12 +10,24 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
         <div className="mb-8 text-xl font-bold">
           Damn, that&apos;s a 404.
         </div>
+        <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
+          {errorMsg}
+        </div>
         <div className="text-sm">
           Press{' '}
           <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
             F12
           </kbd>{' '}
-          and open devtools for more details.
+          and open devtools for more details, and report this issue to{' '}
+          <a
+            className="text-blue-600 hover:text-blue-700 hover:underline"
+            href="https://github.com/lyc8503/onedrive-cf-index-ng/issues"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            onedrive-cf-index-ng issues
+          </a>
+          .
         </div>
       </div>
     </div>
